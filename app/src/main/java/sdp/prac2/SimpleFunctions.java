8c0fd9b9337a050c1ee7ed642deb57c016a12296
MashPash <122250805+MashPash@users.noreply.github.com>
2024-05-19 23:34:56 +0200

    Code for Task 5

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,13 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+
+    public boolean Task5(List<Integer> list) {
+    for (int i = 0; i < list.size() - 1; i++) {
+        if (list.get(i) > list.get(i + 1)) {
+            return false;
+        }
+    }
+    return true;
+    }
 }
