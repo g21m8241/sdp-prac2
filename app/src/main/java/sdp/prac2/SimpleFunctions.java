c302a802b95a185595a9d6684e8e2be6a3d4da0b
Kaizer Makhubo <G20m1710@ict.ru.ac.za>
2024-05-19 12:21:39 +0200

    G20m1710, task 1 initial commit

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,14 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+    //Task 1
+    public int sumElementsByIndexes(List<Integer> a, List<Integer> b){
+        int sum= 0;
+        for (Integer index : b){
+            if(index >=0 && index < a.size()){
+                sum += a.get(index);
+            }
+        }
+        return sum;
+    }
 }
